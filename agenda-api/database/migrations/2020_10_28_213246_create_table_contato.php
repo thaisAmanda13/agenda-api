b1a654d988ab18d2c2515e70639f6fff7c03d487
thaisAmanda13 <thais.amandas18@gmail.com>
2020-10-28 19:06:19 -0300

    adicionando migrations de contato, telefone e endereco

---
+++ agenda-api/database/migrations/2020_10_28_213246_create_table_contato.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableContato extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contato', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nome');
+            $table->string('cpf');
+            $table->string('email');
+            $table->timestamps('dataNascimento');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contato');
+    }
+}
