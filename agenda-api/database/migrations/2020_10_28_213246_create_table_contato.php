0dbd22f8f8fe2b23b85a8e114f1ebbd2053d38fe
thaisAmanda13 <thais.amandas18@gmail.com>
2020-10-28 20:25:17 -0300

    adicionando chave estrangeira

--- agenda-api/database/migrations/2020_10_28_213246_create_table_contato.php
+++ agenda-api/database/migrations/2020_10_28_213246_create_table_contato.php
@@ -20,6 +20,16 @@ class CreateTableContato extends Migration
             $table->string('email')->unique();
             $table->date('dataNascimento');
             $table->timestamps();
+            $table->integer('enderecoId')->unsigned();
+            $table->integer('telefoneId')->unsigned();
+        });
+
+        Schema::table('contato', function($table) {
+            $table->foreign('telefoneId')->references('id')->on('telefone');
+            $table->foreign('enderecoId')->references('id')->on('endereco');
+            //verificar se ta correto isso aqui qnd possivel
+            // $table->foreign('FK_contato_endereco')->references('id')
+            // ->on('endereco')->onDelete('cascade')->on;
         });
     }
 
