6e6c6e2c446e68d2d8eb79ba9dfed05fb343bc5d
thaisAmanda13 <thais.amandas18@gmail.com>
2020-11-04 13:47:02 -0300

    arrumando erro import para controller

--- agenda-api/app/Http/Controllers/Api/ContatoController.php
+++ agenda-api/app/Http/Controllers/Api/ContatoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use app\Models\Contato;
 
 class ContatoController extends Controller
 {
--- agenda-api/routes/api.php
+++ agenda-api/routes/api.php
@@ -2,17 +2,17 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers;
+use App\Http\Controllers\Api;
 
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::namespace('API')->name('api.')->group(function(){
+Route::namespace('Api')->name('api.')->group(function(){
     Route::prefix('/contatos')->group(function(){
 
        Route::get('/',[ContatoController::class, 'index'])->name('contatos');
        Route::get('/{id}',[ContatoController::class,'show'])->name('contatoEspecifico');
-       Route::post('/',[ContatoController::class,'store'])->name('contatoStore');
+       Route::post('/novo-contato',[ContatoController::class,'store'])->name('contatoStore');
     });
 });
